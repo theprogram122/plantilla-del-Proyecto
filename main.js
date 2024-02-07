f0a7ab76ea58e880fed7e554037379de06f0aebb
theprogram122 <157767328+theprogram122@users.noreply.github.com>
2024-02-06 18:02:26 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,69 @@
+// Crea una variable lienzo
+
+
+block_y=1;
+block_x=1;
+
+block_image_width = 350;
+block_image_height = 480;
+
+var bloque_objeto_imagen= "";
+
+// Completa la función nueva_imagen() para agregar una nueva imagen
+function nueva_imagen(get_image)
+{
+	
+
+
+
+
+
+
+	
+
+}
+
+window.addEventListener("keydown", mi_TeclaPulsada);
+
+function mi_TeclaPulsada(e)
+{
+teclaPresionada = e.keyCode;
+console.log(teclaPresionada);
+	// Usa el código de tecla apropiado para agregar la imagen roja
+	if(teclaPresionada ==82 ) 
+	{
+
+
+
+	}
+	// Usa el código de tecla apropiado para agregar la imagen verde
+	if(teclaPresionada == )
+	{
+	
+
+
+	}
+	// Usa el código de tecla apropiado para agregar la imagen amarilla
+	if(teclaPresionada == )
+	{
+
+		
+
+	}
+	// Usa el código de tecla apropiado para agregar la imagen rosa
+	if(teclaPresionada == )
+	{
+	
+
+
+	}
+	// Usa el código de tecla apropiado para agregar la imagen azul
+	if(teclaPresionada == )
+	{
+
+
+
+	}
+	
+}
+
